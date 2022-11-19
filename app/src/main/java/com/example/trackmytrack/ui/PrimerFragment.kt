5e765321fdd130dbfd45fb351cd7354523508c7b
fares <0fareshassan0@gmail.com>
2022-11-19 10:02:37 +0200

    #Processing: - Some utils for the getting permissions functionality.
    #Needed:
    - finish getting permissions functionality
    - snakebar and toasts as notes
    - enable/enabled buttons

--- app/src/main/java/com/example/trackmytrack/ui/PrimerFragment.kt
+++ app/src/main/java/com/example/trackmytrack/ui/PrimerFragment.kt
@@ -1,13 +1,21 @@
 package com.example.trackmytrack.ui
 
+import android.content.Intent
+import android.content.IntentSender
+import android.location.LocationRequest
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.trackmytrack.R
+import com.google.android.material.snackbar.Snackbar
 
 class PrimerFragment : Fragment() {
+    private val TAG = PrimerFragment::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +27,17 @@ class PrimerFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_primer, container, false)
     }
 
+
+    private val resolutionForResult = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        // After the user choose from the Permission-Dialog ↴
+        if (permissions.values.all { it }) {
+            // Now all the 3 permissions are granted
+
+        } else {
+            //todo snakebar
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/trackmytrack/utils/constraints.kt
@@ -0,0 +1,5 @@
+package com.example.trackmytrack.utils
+
+import android.os.Build
+
+val runningQOrLater = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
